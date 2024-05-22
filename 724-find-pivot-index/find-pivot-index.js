bbf1acf5f3dc5de94a5111b4e8c3c5c3b2d01bd1
abdulRaw <97601992+abdulRaw@users.noreply.github.com>
2024-05-22 20:36:23 +0530

    Time: 60 ms (81.72%) | Memory: 51.8 MB (68.63%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    const fHash = {};
+    const bHash={};
+    let j = nums.length;
+    let sumf=0;
+    let sumR =0;
+    // let  mid =(nums.length%2)===0 ? (nums.length/2)+1 : nums.length/2 ;
+    for(i=0;i<nums.length;i++){
+         sumR+=nums[--j];
+    }
+     for(i=0;i<nums.length;i++){
+         sumR-=nums[i];
+        if(sumR===sumf) return i;
+         sumf+=nums[i]
+    }
+    return -1;
+};
