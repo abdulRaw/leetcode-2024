49bd153f9a1628fbf1680c000bf006fc0a6f319b
abdulRaw <97601992+abdulRaw@users.noreply.github.com>
2024-06-06 02:09:30 +0530

    Time: 157 ms (6.00%) | Memory: 58.3 MB (5.00%) - LeetSync

--- 2226-rings-and-rods/rings-and-rods.js
+++ 2226-rings-and-rods/rings-and-rods.js
@@ -5,7 +5,6 @@
 var countPoints = function(rings) {
     const rods = new Array(10).fill("").map(()=>new Array(3).fill(0));
     const addRingToRod = (ring,rodIndex)=>{
-       
         if(ring==="R"){
             rods[rodIndex][0]++
         }else if(ring==="G"){
