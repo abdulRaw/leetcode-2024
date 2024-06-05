8c7d0255f53c3a6784170738041ca4b4408adb03
abdulRaw <97601992+abdulRaw@users.noreply.github.com>
2024-06-06 02:08:50 +0530

    Time: 158 ms (6.00%) | Memory: 58.9 MB (5.00%) - LeetSync

---
+++ 2226-rings-and-rods/rings-and-rods.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} rings
+ * @return {number}
+ */
+var countPoints = function(rings) {
+    const rods = new Array(10).fill("").map(()=>new Array(3).fill(0));
+    const addRingToRod = (ring,rodIndex)=>{
+       
+        if(ring==="R"){
+            rods[rodIndex][0]++
+        }else if(ring==="G"){
+             rods[rodIndex][1]++
+        }else if(ring==="B"){
+             rods[rodIndex][2]++
+        }
+         console.log(ring,rodIndex,rods)
+    }
+   const inputArray = rings.split("");
+   for(let i=0;i<inputArray.length-1;i=i+2)
+   {
+    const ring = inputArray[i];
+    const rod = inputArray[i+1];
+    addRingToRod(ring,Number(rod))
+   }
+
+   //console.log(rods)
+
+   return rods.reduce((acc,rings)=>{
+    if(rings.every(rc=> rc>0)){
+        acc++;
+    }
+    return acc;
+   },0)
+
+
+};
