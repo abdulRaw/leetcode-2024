6ef637d833c02a42a223a62593e6ecd8d4391366
abdulRaw <97601992+abdulRaw@users.noreply.github.com>
2024-06-06 23:19:18 +0530

    Time: 60 ms (61.37%) | Memory: 50.5 MB (96.18%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,23 +3,24 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    const hash = nums.reduce((acc,n)=>{
-        acc[n] = 1+( acc?.[n] ?? 0)
-        return acc;
-    },{});
    
-    const entries  = Object.entries(hash);
-     let maxFreq = entries[0][1] ;
-    let maxFeqVal = entries[0][0];
+   let count = 0 ;
+   let maxFeqVal = 0;
 
-    for(let i = 1;i<entries.length;i++ ){
-        const [val,feq] = entries[i];
-
-        if(feq>maxFreq){
-            maxFreq= feq;
-            maxFeqVal= val;
+    nums.forEach((n)=>{
+        if(count==0){
+            maxFeqVal = n;
         }
-    }
+
+        if(maxFeqVal===n){
+            count++;
+        }else{
+            count--;
+        }
+
+    })
+
+
     return maxFeqVal;
 
 } 
