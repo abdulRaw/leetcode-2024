8931fcedd005f78aac4b578b490844c0174f2194
abdulRaw <97601992+abdulRaw@users.noreply.github.com>
2024-06-08 07:44:58 +0530

    Time: 44 ms (95.68%) | Memory: 50.8 MB (74.07%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.js
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.js
@@ -8,7 +8,7 @@
   if(h>=l){
     let mid = Math.floor((l+h)/2);
     const cr = arr[mid];
-    console.log(cr,mid,l,h);
+   // console.log(cr,mid,l,h);
     if(cr<0){
         return binarySearch(arr,l, mid-1)
     }else {
@@ -20,10 +20,22 @@
  }
 var countNegatives = function(grid) {
     let sum =0;
-    for(let i =0;i<grid.length;i++){
-        const row = grid[i];
+        const row = grid[0];
         const nIdxStart = binarySearch(row,0,row.length-1);
-        if(row[nIdxStart]<0)sum+=row.length-nIdxStart
+        prevIndex=row.length-1;
+        if(row[nIdxStart]<0){
+            sum+=row.length-nIdxStart;
+            prevIndex=prevIndex;
+        }
+    for(let i =1;i<grid.length;i++){
+        const row = grid[i];
+        const nIdxStart = binarySearch(row,0,prevIndex);
+       if(row[nIdxStart]<0){
+            sum+=row.length-nIdxStart;
+            prevIndex=prevIndex;
+        }else {
+              prevIndex=row.length-1;
+        }
     }
     return sum;
 };
