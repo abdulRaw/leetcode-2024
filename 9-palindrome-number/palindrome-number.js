334b0f7804982c6e9fa7b09e1251b6fbd983311b
abdulRaw <97601992+abdulRaw@users.noreply.github.com>
2024-06-08 18:16:30 +0530

    Time: 190 ms (5.01%) | Memory: 59.2 MB (6.45%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+   if(x<0){
+    return false;
+   }
+
+   let rev = 0
+   let n = x;
+
+   while(n>0){
+    rev=(rev*10)+(n%10);
+    n= Math.floor(n/10);
+   }
+
+   console.log(rev)
+
+   return rev==x;
+
+
+
+};
