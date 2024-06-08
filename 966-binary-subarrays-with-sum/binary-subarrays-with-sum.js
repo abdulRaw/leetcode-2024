5054c5930bc69e45211bc44fc8367bac72d6b8be
abdulRaw <97601992+abdulRaw@users.noreply.github.com>
2024-06-08 18:03:38 +0530

    Time: 69 ms (46.61%) | Memory: 54.2 MB (45.76%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.js
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.js
@@ -4,30 +4,23 @@
  * @return {number}
  */
 var numSubarraysWithSum = function(nums, goal) {
-    let count = 0;
-    let prefixSum = 0;
-    let prefixSums = new Map();
-    
-    // Initialize the map with the prefix sum 0
-    prefixSums.set(0, 1);
-    
-    for (let num of nums) {
-        prefixSum += num;
-        
-        // Check if there's a subarray ending at the current index with the required sum
-        if (prefixSums.has(prefixSum - goal)) {
-            count += prefixSums.get(prefixSum - goal);
-        }
-        
-        // Update the map with the current prefix sum
-        if (prefixSums.has(prefixSum)) {
-            prefixSums.set(prefixSum, prefixSums.get(prefixSum) + 1);
-        } else {
-            prefixSums.set(prefixSum, 1);
+    const preFixhash = {0:1};
+    let preFixSum = 0;
+    let freq =0;
+    nums.forEach((n)=>{
+        preFixSum+=n;
+        if(preFixhash[preFixSum-goal]){
+            freq+=preFixhash[preFixSum-goal]
         }
 
-       
-    };
- return count;
+        if(preFixhash[preFixSum]){
+           preFixhash[preFixSum]++;
+        }else {
+            preFixhash[preFixSum]=1;
+        }
+    })
+
+
+ return freq;
 
 }
