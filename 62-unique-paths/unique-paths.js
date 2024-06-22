3e79d58f8c54c8db20ecaeb4254aa2150c08dd7e
abdulRaw <97601992+abdulRaw@users.noreply.github.com>
2024-06-22 19:44:30 +0530

    Time: 60 ms (26.55%) | Memory: 49 MB (64.97%) - LeetSync

--- 62-unique-paths/unique-paths.js
+++ 62-unique-paths/unique-paths.js
@@ -4,30 +4,15 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
+    let aboveRow = Array(n).fill(1);
 
-    const dp = new Array(m+1).fill("").map(()=> new Array(n+1).fill(0))
-    const beakAndFindPaths = (m,n)=>{
-        //check if already computed values is stored in dp matrix
-        if(dp[m][n]){
-            return dp[m][n];
+    for (let row = 1; row < m; row++) {
+        let currentRow = Array(n).fill(1);
+        for (let col = 1; col < n; col++) {
+            currentRow[col] = currentRow[col - 1] + aboveRow[col];
         }
-
-        // if any row or any cols is one we only have one path in such case 
-        else if(m===1 || n===1){
-           dp[m][n] = 1;
-
-           return  dp[m][n]
-        }
-        // stored newly computed values in dp matrix
-        dp[m][n]= beakAndFindPaths(m,n-1)+beakAndFindPaths(m-1,n)
-
-        return dp[m][n]
+        aboveRow = currentRow;
     }
 
-   // console.log(dp)
-
-    //init paths finding algo
-    beakAndFindPaths(m,n)
- console.log(dp)
-    return dp[m][n]
+    return aboveRow[n - 1];    
 };
