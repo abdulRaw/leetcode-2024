c2b4cc78d1621dca4330dcf058836f91f2e33b36
abdulRaw <97601992+abdulRaw@users.noreply.github.com>
2024-07-06 13:44:04 +0530

    Time: 58 ms (57.95%) | Memory: 49.9 MB (80.83%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.js
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.js
@@ -4,6 +4,8 @@
  * @return {boolean[]}
  */
 var kidsWithCandies = function(candies, extraCandies) {
+    
     let max = Math.max(...candies);
     return candies.map((c)=> c+extraCandies>=max);
+    
 };
