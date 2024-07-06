782c1de7e53454e623f4e1c1157fbb4ad72865df
abdulRaw <97601992+abdulRaw@users.noreply.github.com>
2024-07-06 22:53:08 +0530

    Time: 55 ms (33.64%) | Memory: 48.2 MB (93.31%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.js
+++ 70-climbing-stairs/climbing-stairs.js
@@ -5,8 +5,11 @@
 var climbStairs = function(n) {
     const dp = [1,1];
     for(let i = 2;i<=n;i++){
-       dp[i] =  dp[i-1]+dp[i-2];
+        let next =(dp[0]+dp[1]);
+        dp[1] = dp[0];
+        dp[0]= next;
+     
     }
 
-    return dp[n];
+    return dp[0];
 };
