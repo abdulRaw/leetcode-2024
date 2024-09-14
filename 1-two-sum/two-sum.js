14ef6b174730d6cee25038f9234997c42d4fa05f
abdulRaw <97601992+abdulRaw@users.noreply.github.com>
2024-09-14 15:10:35 +0530

    Time: 85 ms (46.66%) | Memory: 57.5 MB (5.22%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -6,25 +6,39 @@
 var twoSum = function(nums, target) {
 
 const result = [];
+const hashMap = {
+//    7: 0,
+//    2: 1, 
+//    -4:2,
+//    -6:3
 
-for(let i =0;i<nums.length;i++)
+
+};
+//  0 1 2  3
+// [2,7,11,15]
+//  I = 2 
+//  j 
+for(let i =0;i<nums.length;i++) // ON
 {
     const a = nums[i];
-    for(let j = i+1;j<nums.length;j++) 
-    {
-        const b = nums[j];
-
-        if(a+b===target)
-        {
-         result.push(i,j)   ;
-        }
-
-    }
+    const rem = target- a;
+    hashMap[rem] = i; 
 
 }
 
+for(let i =0;i<nums.length;i++) 
+{
+     const a = nums[i];
+    if(hashMap[a]!==undefined &&  hashMap[a] !==i){
+        result.push(i, hashMap[a])
+        //delete hashMap[rem]
+        break;
+    }
 
-// console.log(result)
+
+}
+
+ console.log(hashMap)
 
 return result
 }
